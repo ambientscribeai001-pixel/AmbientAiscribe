5216cfb6615e71f26fd2f7634a6f2917f8fd90c7
ambientscribeai001-pixel <ambientscribeai001@gmail.com>
2026-06-20 15:28:01 +0100

    Update routes_auth.js

--- routes_auth.js
+++ routes_auth.js
@@ -5,7 +5,7 @@
 const express    = require('express');
 const jwt        = require('jsonwebtoken');
 const rateLimit  = require('express-rate-limit');
-const User       = require('./middleware_User');
+const User       = require('./User');
 const {
   protect,
   recordFailedAttempt,
